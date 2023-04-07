a510c7731791e9a34e482a270dbc6929c591e3bd
Daniel Farnham <z5115421@ad.unsw.edu.au>
2023-04-07 11:18:15 +1000

    Refactoring of goals for part e

---
+++ src/main/java/dungeonmania/goals/OrGoal.java
@@ -0,0 +1,23 @@
+package dungeonmania.goals;
+
+import dungeonmania.Game;
+
+public class OrGoal implements Goal {
+    private final Goal goal1;
+    private final Goal goal2;
+
+    public OrGoal(Goal goal1, Goal goal2) {
+        this.goal1 = goal1;
+        this.goal2 = goal2;
+    }
+
+    @Override
+    public boolean achieved(Game game) {
+        return goal1.achieved(game) || goal2.achieved(game);
+    }
+
+    @Override
+    public String toString(Game game) {
+        return achieved(game) ? "" : "(" + goal1.toString(game) + " OR " + goal2.toString(game) + ")";
+    }
+}
