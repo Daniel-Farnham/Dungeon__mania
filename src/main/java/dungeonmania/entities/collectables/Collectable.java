23b7381b4f4c9c642ec30c983859bb02b2cfb2d1
Carl Buchanan <carl.buchanan@student.unsw.edu.au>
2023-04-22 16:49:22 +1000

    results

--- src/main/java/dungeonmania/entities/collectables/Collectable.java
+++
@@ -1,8 +0,0 @@
-package dungeonmania.entities.collectables;
-
-import dungeonmania.entities.Player;
-import dungeonmania.map.GameMap;
-
-public interface Collectable {
-    void onPlayerCollect(GameMap map, Player player);
-}
